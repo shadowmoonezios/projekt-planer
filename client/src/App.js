7560330172401375576cf8dba16949b4afbe9f84
shadowmoonezios <agushifelde@hotmail.com>
2025-11-07 14:46:47 +0200

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Projekt Planer</h1>
+      <p>Bitte melde dich an, um fortzufahren.</p>
+    </div>
+  );
+}
+
+export default App;
